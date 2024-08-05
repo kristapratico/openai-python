99a21f25508830170c325ac4e10c34413f960119
Krista Pratico <krpratic@microsoft.com>
2024-08-05 12:56:47 -0700

    add tests

--- src/openai/_models.py
+++ src/openai/_models.py
@@ -608,6 +608,7 @@ def _build_discriminated_union_meta(*, union: type, meta_annotations: tuple[Any,
                 if hasattr(field_info, "annotation"):
                     field_annotation = cast(type, field_info.annotation)
                 else:
+                    # pydantic==1.9
                     field_annotation = cast(type, field_info.outer_type_)
                 if field_annotation and is_literal_type(field_annotation):
                     for entry in get_args(field_annotation):
